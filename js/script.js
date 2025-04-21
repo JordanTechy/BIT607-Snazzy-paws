2fe4a6182dc60aca514fc799f2a0c9f58b98cebd
SSProdManager <nzsonasafe.prodmanager@gmail.com>
2025-04-22 08:02:08 +1200

    Nav bar done :)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("test").textContent = "From my javascript file!";
+
+  // this is to make the hamburger menue open the nav links on the mobile
+  const hamburger = document.querySelector(".hamburger");
+  const navLinkBox = document.querySelector(".nav-link-box");
+
+  hamburger.addEventListener("click", () => {
+    console.log("Hamburger clicked!");
+    navLinkBox.classList.toggle("active");
+  });
 });
